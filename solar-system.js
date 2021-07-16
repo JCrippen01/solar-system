b9683bb248ee48a2898b6a44cdb98db1126c4b91
Scott47 <scottsilver47@gmail.com>
2021-07-16 10:00:20 -0500

    moved celestial objects into solarSystem array and logged planets and stars to the console

--- solar-system.js
+++ solar-system.js
@@ -1,33 +1,36 @@
 
-const solarSystem = []
+const solarSystem = [
+    {
+        id: 1,
+        name: "Sun",
+        star: true,
+        numberOfMoons: 0,
+        distanceFromSun: 0,
+        mass: 1.98e+30
+    },
+    {
+        id: 2,
+        name: "Mercury",
+        star: false,
+        numberOfMoons: 0,
+        distanceFromSun: 48.47,
+        mass: 3.285e+23
+    },
+    {
+        id: 3,
+        name: "Earth",
+        star: false,
+        numberOfMoons: 1,
+        distanceFromSun: 147.5,
+        mass: 5.97e+24
+    }
+]
 
-const sun = {
-    id: 1,
-    name: "Sun",
-    star: true,
-    numberOfMoons: 0,
-    distanceFromSun: 0,
-    mass: 1.98e+30
+for (const planet of solarSystem) {
+    if (planet.star === false) {
+        
+        console.log(`${planet.name} is a planet and not a star`)
+    } else {
+        console.log(`${planet.name} is a star and not a planet`)
+    }
 }
-
-const mercury = {
-    id: 2,
-    name: "Mercury",
-    star: false,
-    numberOfMoons: 0,
-    distanceFromSun: 48.47,
-    mass: 3.285e+23
-}
-
-const earth = {
-    id: 3,
-    name: "Earth",
-    star: false,
-    numberOfMoons: 1,
-    distanceFromSun: 147.5,
-    mass: 5.97e+24
-}
-
-console.log(earth, "earth")
-console.log(mercury, "mercury")
-console.log(sun, "sun")
