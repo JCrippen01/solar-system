087bb2090ee8524f4ac7ffc75d8cc11451947ff1
Scott47 <scottsilver47@gmail.com>
2021-07-16 09:34:33 -0500

    added three objects to solar system

---
+++ solar-system.js
@@ -0,0 +1,33 @@
+
+const solarSystem = []
+
+const sun = {
+    id: 1,
+    name: "Sun",
+    star: true,
+    numberOfMoons: 0,
+    distanceFromSun: 0,
+    mass: 1.98e+30
+}
+
+const mercury = {
+    id: 2,
+    name: "Mercury",
+    star: false,
+    numberOfMoons: 0,
+    distanceFromSun: 48.47,
+    mass: 3.285e+23
+}
+
+const earth = {
+    id: 3,
+    name: "Earth",
+    star: false,
+    numberOfMoons: 1,
+    distanceFromSun: 147.5,
+    mass: 5.97e+24
+}
+
+console.log(earth, "earth")
+console.log(mercury, "mercury")
+console.log(sun, "sun")
